ef355ed177c42b8dd9f1e21e912ae84539b45c19
Rajiv Bose <rbose85@users.noreply.github.com>
2014-05-25 16:12:14 +0100

    make default db settings an update of semi-completed values

--- backend/les/settings/local.py
+++ backend/les/settings/local.py
@@ -17,11 +17,8 @@ ALLOWED_HOSTS = [SITE_DOMAIN, ]
 
 #### DJANGO: django.conf.global_settings
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'lesbose_db',
-        'USER': 'lesbose_user',
-        'PASSWORD': get_env_setting('DATABASE_LOCAL_PASSWORD')
-    }
-}
+DATABASES['default'].update({
+    'NAME': 'lesbose_db',
+    'USER': 'lesbose_user',
+    'PASSWORD': get_env_setting('DATABASE_PASSWORD')
+})
