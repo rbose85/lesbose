3249bcbc1efb4d776bc9ffe3cb76983d840f6ddb
Rajiv Bose <rajiv@lesbose.com>
2014-05-25 16:58:54 +0100

    introduce heroku specific runtime logic

--- backend/les/wsgi.py
+++ backend/les/wsgi.py
@@ -3,7 +3,11 @@ WSGI config for project.
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "les.settings.local")
 
 from django.core.wsgi import get_wsgi_application
+
+
+if os.environ.get('HOST_PLATFORM') in ['heroku', ]:
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'les.settings.heroku')
+
 application = get_wsgi_application()
