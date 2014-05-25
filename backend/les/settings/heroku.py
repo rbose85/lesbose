975ed9ed259fd70222945ab3a4f9e3adee803c23
Rajiv Bose <rbose85@users.noreply.github.com>
2014-05-25 16:15:33 +0100

    add heroku version of settings based on local

---
+++ backend/les/settings/heroku.py
@@ -0,0 +1,26 @@
+from .base import *
+
+
+#### PROJECT
+
+PROJECT = 'lesbose-heroku'
+SITE_DOMAIN = 'api.lesbose.com'
+HEROKU_DOMAIN = 'lesbose.herokuapp.com'
+
+
+#### DJANGO
+
+DEBUG = True
+TEMPLATE_DEBUG = True
+
+ALLOWED_HOSTS = [HEROKU_DOMAIN, SITE_DOMAIN, ]
+
+
+#### DJANGO: django.conf.global_settings
+
+DATABASES['default'].update({
+    'NAME': get_env_setting('DATABASE_NAME'),
+    'USER': get_env_setting('DATABASE_USER'),
+    'PASSWORD': get_env_setting('DATABASE_PASSWORD'),
+    'HOST': get_env_setting('DATABASE_HOST'),
+})
