3ccfad39c3aceaa878bbf61ba7cc2e6608095b9a
Rajiv Bose <rajiv@lesbose.com>
2014-05-25 17:20:12 +0100

    initial work with heroku shows DJANGO_SETTINGS_MODULE env var works

--- backend/les/wsgi.py
+++ backend/les/wsgi.py
@@ -2,12 +2,6 @@
 WSGI config for project.
 """
 
-import os
-
 from django.core.wsgi import get_wsgi_application
-
-
-if os.environ.get('HOST_PLATFORM') in ['heroku', ]:
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'les.settings.heroku')
 
 application = get_wsgi_application()
