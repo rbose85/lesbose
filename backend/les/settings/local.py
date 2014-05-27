a09d2836c4d8bbb8a3687b1441e27314f370a911
Rajiv Bose <rbose85@users.noreply.github.com>
2014-05-27 20:39:44 +0100

    add logging to project, including logging for custom commands

--- backend/les/settings/local.py
+++ backend/les/settings/local.py
@@ -27,3 +27,57 @@ DATABASES['default'].update({
 INSTALLED_APPS += (
     'les',
 )
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+
+    'formatters': {
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+        'verbose': {
+            'format': "%(levelname)s [%(name)s:%(lineno)s] %(message)s"
+        },
+    },
+
+    'handlers': {
+        'console-simple': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple',
+        },
+        'console-verbose': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose',
+        },
+    },
+
+    'loggers': {
+
+        #### D J A N G O ####
+        'django': {
+            'handlers': ['console-verbose'],
+            'level': 'INFO',
+            'propagate': False,
+        },
+        'django.db.backends': {
+            'handlers': ['console-verbose', ],
+            'level': 'INFO',
+            'propagate': False,
+        },
+
+        #### P R O J E C T ####
+        '': {
+            'handlers': ['console-simple'],
+            'level': 'INFO',
+            'propagate': False,
+        },
+        'les.management.commands': {
+            'handlers': ['console-simple'],
+            'level': 'INFO',
+            'propagate': False,
+        },
+    }
+}
