9f3fd0ee251514fee5fd76a0c28048005990b653
Rajiv Bose <rbose85@users.noreply.github.com>
2014-05-27 20:48:38 +0100

    add custom management command, 'lesfakes'

--- backend/core/models.py
+++ backend/core/models.py
@@ -13,3 +13,19 @@ class TimeStampModel(models.Model):
         get_latest_by = 'modified'
         ordering = ('-modified', '-created',)
         abstract = True
+
+
+# todo: if this pattern takes-off, remove this out into a proj of its own
+class AbstractBaseFakes(object):
+    """
+    Abstract base class. Each concrete subtype will live in an app's 'fakes.py'.
+    """
+
+    class Meta:
+        abstract = True
+
+    def execute(self):
+        """
+        Override this func, it will be called to execute the fake.
+        """
+        raise NotImplementedError()
