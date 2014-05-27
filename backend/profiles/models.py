f21734e27266fda4e992a74526d6dcd9cf610800
Rajiv Bose <rajiv@lesbose.com>
2014-05-27 11:09:38 +0100

    add new profiles app to project

---
+++ backend/profiles/models.py
@@ -0,0 +1,18 @@
+from django.conf import settings
+from django.db import models
+
+from core.models import TimeStampModel
+
+
+class AccountHolder(TimeStampModel):
+    """
+    Define the owner of an account.
+    """
+
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
+    first_name = models.CharField('Forename', max_length=30, blank=False,
+                                  default='')
+    last_name = models.CharField('Surname', max_length=30, blank=False,
+                                 default='')
+    date_of_birth = models.DateField('Date of Birth', null=True, blank=True,
+                                     default=None)
