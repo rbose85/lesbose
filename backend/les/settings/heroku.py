c26c321c2a197d062f987a719b605997ef05e5cc
Rajiv Bose <rajiv@lesbose.com>
2014-06-05 21:37:02 +0100

    update LOGGING for heroku settings

--- backend/les/settings/heroku.py
+++ backend/les/settings/heroku.py
@@ -25,6 +25,58 @@ INSTALLED_APPS += (
     'gunicorn',
 )
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+
+    'formatters': {
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+        'verbose': {
+            'format': ('%(asctime)s [%(process)d] [%(levelname)s] ' +
+                       'pathname=%(pathname)s lineno=%(lineno)s ' +
+                       'funcname=%(funcName)s %(message)s'),
+            'datefmt': '%Y-%m-%d %H:%M:%S',
+        },
+    },
+
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose',
+        },
+    },
+
+    'loggers': {
+
+        #### D J A N G O ####
+        'django': {
+            'handlers': ['console'],
+            'level': 'INFO',
+            'propagate': False,
+        },
+        'django.db.backends': {
+            'handlers': ['console', ],
+            'level': 'INFO',
+            'propagate': False,
+        },
+
+        #### P R O J E C T ####
+        'les.management.commands': {
+            'handlers': ['console'],
+            'level': 'INFO',
+            'propagate': False,
+        },
+        '': {
+            'handlers': ['console'],
+            'level': 'INFO',
+            'propagate': False,
+        },
+    }
+}
+
 SESSION_COOKIE_SECURE = True
 
 CSRF_COOKIE_SECURE = True
