665f879a7f126d13fc9645483ffd0a7fc0574317
Rajiv Bose <rbose85@users.noreply.github.com>
2014-06-12 20:28:10 +0100

    hotfix: variable typo

--- backend/les/settings/base.py
+++ backend/les/settings/base.py
@@ -59,7 +59,7 @@ INSTALLED_APPS += (
 
 SECRET_KEY = get_env_setting('SECRET_KEY')
 
-STATIC_ROOT = os.path.join(PROJECT_PATH, 'staticfiles')
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 
